6e03412913339fd468f23fa8ad9b8fe314b0d4f9
Javier <javieralzuu@gmail.com>
2024-08-29 21:54:26 -0300

    form fix

--- script.js
+++ script.js
@@ -33,3 +33,16 @@ $(document).ready(function(){
            ]
     });
 });
+
+
+
+jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener('touchstart', handle, { passive: !ns.includes('noPreventDefault') });
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener('touchmove', handle, { passive: !ns.includes('noPreventDefault') });
+    }
+};
